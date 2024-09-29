33febafec85f7678ff1ee4f2198df8f0eb9a6ecf
Bartłomiej Strama <kontakt@bstrama.com>
2024-09-29 02:14:48 +0200

    elastic search

--- actions/user-data.ts
+++ actions/user-data.ts
@@ -2,6 +2,8 @@
 import {currentUser} from "@/lib/auth";
 import {db} from "@/lib/db";
 
+import {userPublicFields} from "@/lib/userPublicFields";
+
 export async function getUser() {
     const user = await currentUser();
     if (!user) {
@@ -34,17 +36,7 @@ export async function getUser() {
 export async function getPublicUserData(userId: string) {
     const userData = await db.user.findUnique({
         where: {id: userId},
-        select: {
-            id: true,
-            email: true,
-            name: true,
-            description: true,
-            type: true,
-            image: true,
-            tags: true,
-            updatedAt: true,
-            createdAt: true,
-        }
+        select: userPublicFields,
     });
     if (!userData) {
         return {error: "User data not found!"};
--- actions/user-list.ts
+++ actions/user-list.ts
@@ -2,29 +2,69 @@
 
 import {db} from "@/lib/db";
 import {UserType} from "@/actions/fulfill-user-data";
+import {ElasticIndexes, elasticSearch} from "@/lib/elasticSearch";
+import {currentUser} from "@/lib/auth";
 
-export function getUserList(take: number, skip: number, userType: UserType | UserType[]) {
+export async function getUserList(take: number, skip: number, userType: UserType | UserType[]) {
     const type = Array.isArray(userType) ? userType : [userType];
+    const user = await currentUser();
 
-    return db.user.findMany({
-        take,
-        skip,
-        where: {
-            type: {in: type},
-            isFulfilled: true,
-            emailVerified: {
-                not: null
+    const recommendedUsersPromise = elasticSearch.search({
+        index: ElasticIndexes.UserIndex,
+        query: {
+            bool: {
+                must: {
+                    terms: {type},
+                },
+                filter: {
+                    term: {
+                        isFulfilled: true,
+                    },
+                },
+                should: {
+                    more_like_this: {
+                        fields: ["name", "description", "tags"],
+                        like: [
+                            {
+                                _index: ElasticIndexes.UserIndex,
+                                _id: user?.id,
+                            },
+                        ],
+                        min_term_freq: 1,
+                        min_doc_freq: 1,
+                    },
+                },
             },
         },
-        select: {
-            id: true,
-            email: true,
-            name: true,
-            description: true,
-            image: true,
-            tags: true,
-            updatedAt: true,
-            createdAt: true,
-        }
+        size: take,
+        from: skip,
     });
+
+    let elasticUserIds: string[] = [];
+    try {
+        elasticUserIds = (await recommendedUsersPromise).hits.hits
+            .map((hit) => hit._id)
+            .filter((id) => typeof id === "string");
+
+    } catch (error: any) {
+        // if statusCode is not 404, throw the error
+        if (!error?.meta || error.meta.statusCode !== 404) {
+            throw error;
+        }
+    }
+
+    return db.$queryRaw`
+      SELECT "id", "email", "name", "description", "type", "image", "tags", "updatedAt", "createdAt"
+      FROM "User"
+      WHERE "type"::text = ANY(${type})
+        AND "isFulfilled" = true 
+        AND "emailVerified" IS NOT NULL
+      ORDER BY
+        CASE
+          WHEN "id" = ANY(${elasticUserIds}) THEN 1
+          ELSE 2
+        END,
+        "id" ASC
+      LIMIT ${take} OFFSET ${skip}
+    `;
 }
