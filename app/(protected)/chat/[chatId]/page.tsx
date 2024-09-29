bc3e8d3951212f329f7ada9067eb9a28eb71478c
Jakub Grzybek <jakubgrzybekinf@gmail.com>
2024-09-29 10:21:40 +0200

    modify chat

--- app/(protected)/chat/[chatId]/page.tsx
+++ app/(protected)/chat/[chatId]/page.tsx
@@ -1,75 +1,72 @@
-"use client";;
-import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
-import { Input } from "@/components/ui/input";
-import { Button } from "@/components/ui/button";
-import { MdSend } from "react-icons/md";
-import { Separator } from "@/components/ui/separator";
+"use client";
+import {Card, CardContent, CardHeader, CardTitle} from "@/components/ui/card";
+import {Input} from "@/components/ui/input";
+import {Button} from "@/components/ui/button";
+import {MdSend} from "react-icons/md";
+import {Separator} from "@/components/ui/separator";
 import TextMessage from "@/app/(protected)/chat/[chatId]/textMessage";
 import MoneyRequestMessage from "@/app/(protected)/chat/[chatId]/moneyRequestMessage";
-import RequirementsMessage from "@/app/(protected)/chat/[chatId]/requirementsMessage";
-import { useCallback, useEffect, useState } from "react";
-import { useParams } from "next/navigation";
-import { getPublicUserData } from "@/actions/user-data";
-import { sendMessage } from "@/actions/send-message";
-import { getMessages } from "@/actions/get-messages";
-
-let socket;
-
+import {useCallback, useEffect, useState} from "react";
+import {useParams} from "next/navigation";
+import {getPublicUserData} from "@/actions/user-data";
+import {sendMessage} from "@/actions/send-message";
+import {getMessages} from "@/actions/get-messages";
+import {Skeleton} from "@/components/ui/skeleton";
 
 export interface Message {
-    id: string
-    content: string,
-    isYourMessage: boolean,
-    updatedAt?: string,
-    type: "TEXT" | "requirements" | "moneyRequest",
-    status?: "accepted" | "rejected" | "pending",
-    requirements?: Array<{
-        id: number,
-        name: string,
-        description: string
-    }>,
-    money?: number
+    id: string,
+    content: string
+    isYourMessage: boolean
+    type: "TEXT" | "CONTRACT"
+    updatedAt: Date,
+    contract?: any
 }
 
-interface IChatData {
-    messages: Message[],
-    recipient: string
+interface ChatData {
+    data: Message[]
+    recipient?: string
 }
 
 
 export default function ChatPage() {
     const params = useParams();
 
-    const [chatData, setChatData] = useState<IChatData | null>(null);
+    const [chatData, setChatData] = useState<ChatData | null>({
+        data: [],
+        recipient: ""
+    });
     const [messageContent, setMessageContent] = useState<string>("");
 
 
     const fetchUser = useCallback(() => {
+        let messages: any = [];
+        getMessages(params.chatId as string).then((data) => {
+            if (!data) return;
+            messages = data.data!;
+        });
         getPublicUserData(params.chatId as string).then((data) => {
             if (!data) return;
             setChatData({
-                messages: [],
+                data: chatData?.data && chatData!.data.length === 0 ? messages : chatData!.data,
                 recipient: data.data!.name || "",
             });
         });
     }, [params.chatId]);
 
-    useEffect(() => {
-        fetchUser();
-    }, [fetchUser]);
-
-    
     const fetchMessages = useCallback(() => {
         getMessages(params.chatId as string).then((data) => {
             if (!data) return;
-            console.log(data);
             setChatData({
-                messages: data.data!,
+                data: data.data!,
                 recipient: chatData!.recipient,
             });
         });
     }, [params.chatId, chatData]);
-    
+
+    useEffect(() => {
+        fetchUser();
+    }, [fetchUser]);
+
     useEffect(() => {
         //fetch every 5 seconds
         const interval = setInterval(() => {
@@ -79,17 +76,19 @@ export default function ChatPage() {
     }, [fetchMessages]);
 
     const sendMessageHandler = () => {
-        setChatData({
-            messages: [
-                ...chatData!.messages,
-                {
-                    id: String(Math.random()),
-                    content: messageContent,
-                    isYourMessage: true,
-                    type: "TEXT"
-                }
-            ],
-            recipient: chatData!.recipient
+        setChatData((prevState: any) => {
+            return {
+                data: [
+                    ...prevState!.data,
+                    {
+                        id: String(Math.random()),
+                        content: messageContent,
+                        isYourMessage: true,
+                        type: "TEXT"
+                    }
+                ],
+                recipient: chatData!.recipient
+            }
         });
         setMessageContent("");
         sendMessage({
@@ -99,78 +98,34 @@ export default function ChatPage() {
         });
     };
 
-    // const chatData: IChatData = {
-    //     messages: [
-    //         {
-    //             id: 1,
-    //             content: "Witaj, jesteśmy chętni podjąć współpracę.",
-    //             isYourMessage: true,
-    //             type: "text"
-    //         }, {
-    //             id: 2,
-    //             content: "Cześć, to są nasze wymagania biznesowe.",
-    //             requirements: [
-    //                 {
-    //                     id: 1,
-    //                     name: "Wymaganie 1",
-    //                     description: "Opis wymagania 1"
-    //                 },
-    //                 {
-    //                     id: 2,
-    //                     name: "Wymaganie 2",
-    //                     description: "Opis wymagania 2"
-    //                 },
-    //                 {
-    //                     id: 3,
-    //                     name: "Wymaganie 3",
-    //                     description: "Opis wymagania 3"
-    //                 }
-    //             ],
-    //             isYourMessage: false,
-    //             type: "requirements"
-    //         }, {
-    //             id: 3,
-    //             content: "Akceptujemy warunki, jednakże potrzebujemy dotacji.",
-    //             type: "moneyRequest",
-    //             isYourMessage: true,
-    //             money: 10000
-    //         }
-    //     ],
-    //     recipient: "Blachotrapez"
-    // }
-
-    if (!chatData) return <div>Ładowanie...</div>
-
     return (
         <main className="py-10 px-5 max-w-[1400px] mx-auto flex flex-col gap-4">
             <Card className="px-10 py-5 min-h-[600px]">
                 <CardHeader>
-                    <CardTitle className="text-2xl">Czat z {chatData.recipient}</CardTitle>
+                    <CardTitle className="text-2xl">Czat z {chatData!.data.length > 0 ? chatData!.recipient : "użytkownik"}</CardTitle>
                 </CardHeader>
-                <Separator />
+                <Separator/>
                 <CardContent className="flex flex-col gap-4 py-4 overflow-scroll">
                     {
-                        chatData.messages.map((message) => {
+                        chatData!.data.length > 0 ? chatData!.data.map((message) => {
 
                             if (message.type === "TEXT") {
-                                return <TextMessage key={message.id} message={message} />
+                                return <TextMessage key={message.id} message={message}/>
                             }
 
-                            if (message.type === "requirements") {
-                                return <RequirementsMessage key={message.id} message={message} />
+
+                            if (message.type === "CONTRACT") {
+                                return <MoneyRequestMessage key={message.id} message={message}/>
                             }
 
-                            if (message.type === "moneyRequest") {
-                                return <MoneyRequestMessage key={message.id} message={message} />
-                            }
-
-                        })
+                        }) : <Skeleton className="w-[100px] h-[20px] rounded-full"/>
                     }
                 </CardContent>
             </Card>
             <Card className="flex flex-row gap-4 p-4">
-                <Input placeholder="Wpisz wiadomość..." value={messageContent} onChange={(e) => setMessageContent(e.target.value)} />
-                <Button className="bg-red-700 hover:bg-red-500" onClick={sendMessageHandler}><MdSend /></Button>
+                <Input placeholder="Wpisz wiadomość..." value={messageContent}
+                       onChange={(e) => setMessageContent(e.target.value)}/>
+                <Button className="bg-red-700 hover:bg-red-500" onClick={sendMessageHandler}><MdSend/></Button>
             </Card>
         </main>
     )
