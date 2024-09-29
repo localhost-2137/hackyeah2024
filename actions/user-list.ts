ebed7d0c251d99ff0519739ae34e48831911880c
Maksymilian Gala <maksymilian.gala1@gmail.com>
2024-09-29 02:32:25 +0200

    fix: conflict

--- actions/user-list.ts
+++ actions/user-list.ts
@@ -5,10 +5,10 @@ import {UserType} from "@/actions/fulfill-user-data";
 import {ElasticIndexes, elasticSearch} from "@/lib/elasticSearch";
 import {currentUser} from "@/lib/auth";
 
-<<<<<<< HEAD
-export async function getUserList(take: number, skip: number, userType: UserType | UserType[]) {
-    const type = Array.isArray(userType) ? userType : [userType];
+export async function getUserList(take: number, skip: number, userType?: UserType) {    
+    const type = userType ? [userType] : [];
     const user = await currentUser();
+    
 
     const recommendedUsersPromise = elasticSearch.search({
         index: ElasticIndexes.UserIndex,
@@ -68,37 +68,4 @@ export async function getUserList(take: number, skip: number, userType: UserType
         "id" ASC
       LIMIT ${take} OFFSET ${skip}
     `;
-}
-=======
-export async function getUserList(take: number, skip: number, userType?: UserType) {    
-    const type = userType ? [userType] : [];
-    const whereParams: any = {
-        isFulfilled: true,
-        emailVerified: {
-            not: null
-        },
-    };
-    if (type.length > 0) {
-        whereParams['type'] = {
-            in: type
-        };
-    }
-
-    const users = await db.user.findMany({
-        take,
-        skip,
-        where: whereParams,
-        select: {
-            id: true,
-            email: true,
-            name: true,
-            description: true,
-            image: true,
-            tags: true,
-            updatedAt: true,
-            createdAt: true,
-        }
-    });
-    return users;
-}
->>>>>>> 280004f0f4b1a0658327625c243e95da41250680
+};
