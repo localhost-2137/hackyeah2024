b6879d0f353eb935de9c515c42450ff1179df95c
Jakub Grzybek <jakubgrzybekinf@gmail.com>
2024-09-29 05:13:24 +0200

    add conversations page with dummy data

---
+++ app/(protected)/chat/page.tsx
@@ -0,0 +1,47 @@
+import {Card, CardContent, CardHeader, CardTitle} from "@/components/ui/card";
+import {Separator} from "@/components/ui/separator";
+
+export default function ChatPage() {
+
+    const conversations = [
+        {
+            id: 1,
+            recipient: "Blachotrapez",
+            lastMessage: "Cześć, czy mogę zamówić 10 ton blachy?",
+        },
+        {
+            id: 2,
+            recipient: "EkoBuda",
+            lastMessage: "Witam, czy 10 000 pln wystarczy na realizacje?",
+        },
+        {
+            id: 3,
+            recipient: "Czysta Energia",
+            lastMessage: "Dziękujemy za wykonanie zlecenia",
+        }
+    ]
+
+    return (
+        <main className="py-10">
+            <Card className="max-w-[1400px] mx-auto px-4">
+                <CardHeader>
+                    <CardTitle className="text-2xl">Prowadzone konwersacje</CardTitle>
+                </CardHeader>
+                <Separator/>
+                <CardContent>
+                    <div className="flex flex-col gap-4 py-4">
+                        {conversations.map((conversation) => (
+                            <Card key={conversation.id}
+                                  className="cursor-pointer py-2 px-6 transition hover:shadow-none hover:text-red-700">
+                                <CardContent className="flex flex-col justify-center p-2">
+                                    <h3 className="text-xl font-semibold">{conversation.recipient}</h3>
+                                    <p className="text-gray-500">Ostatnia aktywność: {conversation.lastMessage}</p>
+                                </CardContent>
+                            </Card>
+                        ))}
+                    </div>
+                </CardContent>
+            </Card>
+        </main>
+    )
+}
